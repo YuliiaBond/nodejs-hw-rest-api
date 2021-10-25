5eb183298c001a75f3932e9aaeb83fdd9064b120
Yuliia Bondarenko <bondarenko047@gmail.com>
2021-10-26 00:16:18 +0300

    share code

---
+++ controllers/contacts/addContact.js
@@ -0,0 +1,13 @@
+const { Contact } = require('../../models')
+
+const addContact = async (req, res) => {
+  const contact = await Contact.create(req.body)
+  res.status(201).json({
+    status: 'success',
+    code: 201,
+    data: {
+      contact
+    }
+  })
+}
+module.exports = addContact
